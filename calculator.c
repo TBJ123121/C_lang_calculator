c2e8ceb66f70e0876197e468493f3a1e3391fb2a
TBJ123121 <61837289+TBJ123121@users.noreply.github.com>
2021-07-19 13:43:06 +0800

    Add files via upload

--- calculator.c
+++ calculator.c
@@ -1,105 +1,39 @@
-// add_op := + | -
-//   mul_op := * | /
-//   digits := {+|-} [0..9] {[0..9]}
-//   expr   := term {add_op term}
-//   term   := factor {mul_op factor}
-//   factor := digits | '(' expr ')'
-
 
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
 
-typedef int calint;
-calint expr(void);
-char token;
-
-void match(char expected)
-{
-    if(token==expected)
-    {
-        token = getchar();
-        return;
-    }
-    fprintf(stderr,"Expected %c, got %c", expected, token);
-    exit(1);
+double factor(char *expr, char **end) {
+    double exp(char*, char**);
+    double res=0;
+    if (*(*end=expr) == '(') {
+        res = exp(*end+1, end);
+        if (**end == ')') ++*end;
+    } else { res=strtod(*end, end); }
+    return res;
 }
 
-void error(const char *msg) {
-    fputs(msg, stderr);
-    exit(1);
+double term(char* expr, char** end) {
+    double res = factor(expr, end);
+    while (**end=='*' || **end=='/') 
+        (**end=='*')? (res*=factor(*end+1, end)): (res/=factor(*end+1, end));
+    return res;
 }
 
-calint factor()
-{
-    calint value;
-    if(token=='(')
-    {
-        match('(');
-        value = expr();
-        match(')');
-    }
-    else if(isdigit(token)||token=='+'||token=='-')
-    {
-        ungetc(token, stdin);
-        scanf("%d", &value);
-
-        token = getchar();
-    }
-    else 
-    {
-        error("error");
-    }
-    return value;
+double exp(char* expr, char** end) { 
+    double res = term(expr, end);
+    while (**end=='+' || **end=='-') 
+        res +=(**end=='+')? term(*end+1, end): -term(*end+1, end);
+    return res;
 }
 
-calint term()
-{
-    calint value = factor();
-    while(token=='*'||token=='/')
+int main(int argc, char *argv[]) {
+    char expr[128]={0}, *pos=0;
+    int i;
+    for (i=1;i<argc;i++)
     {
-        switch(token)
-        {
-            case '*':
-                match('*');
-                value*=factor();
-                break;
-            case '/':
-                match('/');
-                value/=factor();
-                break;
-            default:
-                error("error");
-        }
+        strcat(expr,argv[i]);
     }
-    return value;
-}
-
-calint expr()
-{
-    calint value = term();
-    if(token=='+'||token=='-')
-    {
-        switch(token)
-        {
-            case '+':
-                match('+');
-                value+=term();
-                break;
-            case '-':
-                match('-');
-                value-=term();
-                break;
-            default:
-            error("error");
-        }
-    }
-    return value;
-}
-
-int main(int argc, char *argv[])
-{
-    token = getchar();
-    calint result = expr();
-    printf("reslut: %d\n",result);
+    // printf("Input : ");
+    // scanf("%s", expr);
+    printf("Ans : %f\n", exp(expr, &pos));
 }
